3e994c50c98248401b2a7fdef0970c2c5f802338
levy002 <levy.ukwi002@gmail.com>
2022-04-30 22:18:04 +0200

    Connectin redux store to react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import Navbar from './components/navbar/Navbar';
 import './index.css';
 import BookList from './components/pages/BookList/BookList';
@@ -8,13 +10,15 @@ import Categories from './components/pages/categories/Categories';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<BookList />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  </Provider>,
 );
