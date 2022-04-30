24911a37be68d83ef8a78e35c4313e61927eea57
levy002 <levy.ukwi002@gmail.com>
2022-04-30 22:21:54 +0200

    Creating action and reducres and store

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -2,22 +2,22 @@ const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 const initialState = [];
 
-export const addingBookAction = (book) => ({ type: ADD_BOOK, payload: { book } });
+export const addBook = (book) => ({ type: ADD_BOOK, payload: book });
 
-export const removingBookAction = (id) => ({ type: REMOVE_BOOK, id });
+export const removeBook = (id) => ({ type: REMOVE_BOOK, id });
 
-const bookReducer = (state = initialState, action) => {
+const bookReducer = (Books = initialState, action) => {
   if (action.type === ADD_BOOK) {
-    return [...state,
+    return [...Books,
       action.payload,
     ];
   }
 
   if (action.type === REMOVE_BOOK) {
-    return state.filter((t) => t.id !== action.id);
+    return Books.filter((t) => t.id !== action.id);
   }
 
-  return state;
+  return Books;
 };
 
 export default bookReducer;
