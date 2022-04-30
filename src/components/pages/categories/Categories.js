ee861bae22e112fb6bb08212f326d0fe35608e40
levy002 <levy.ukwi002@gmail.com>
2022-04-30 22:23:03 +0200

    Consuming store and dispatching status reducer

--- src/components/pages/categories/Categories.js
+++ src/components/pages/categories/Categories.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { statusCheck } from '../../../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <button type="button">CHECK STATUS</button>
-  </div>
-);
+const Categories = () => {
+  const state = useSelector((status) => status.statusReducer);
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(statusCheck());
+  };
+
+  return (
+    <>
+      <button type="button" onClick={handleClick}>CHECK STATUS</button>
+      <p>{state}</p>
+    </>
+  );
+};
 
 export default Categories;
