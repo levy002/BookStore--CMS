c913647aded233cd2dad9cfee134c8f3127c2200
levy002 <levy.ukwi002@gmail.com>
2022-05-03 11:16:15 +0200

    Render all books on the page

--- src/components/pages/BookList/BookList.js
+++ src/components/pages/BookList/BookList.js
@@ -1,34 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import Book from '../../book/Book';
 import Form from '../../form/Form';
 
-const BookList = () => {
-  const Books = useSelector((Books) => Books.bookReducer);
-
-  return (
-    <>
-      <ul>
-        {Books.length !== 0 ? (
-          Books.map((book) => (
-            <li key={book.id}>
-              <Book
-                title={book.title}
-                author={book.author}
-                percentage={Math.floor(Math.random() * 100)}
-                id={book.id}
-              />
-            </li>
-          ))
-        ) : (
-          <li>
-            <h2>No Books Availabe!!</h2>
-          </li>
-        )}
-      </ul>
-      <Form />
-    </>
-  );
-};
+const BookList = () => (
+  <>
+    <section>
+      <Book />
+    </section>
+    <Form />
+  </>
+);
 
 export default BookList;
